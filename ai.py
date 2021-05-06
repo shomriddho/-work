bc6bd22a99e33ceb7570b5725c0fc2bb9059b008
shomriddho <82408242+shomriddho@users.noreply.github.com>
2021-05-05 21:56:44 -0700

    done

--- ai.py
+++ ai.py
@@ -1,2 +1,12 @@
 import pyttsx3
 import PyPDF2
+
+book = open('opp.pdf.pdf','rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+page = pdfReader.getPage(7)
+text = page.extractText()
+speaker.say(text)
+speaker.runAndWait()
